4467ffba661c2f9ba5fdb1d4fe9d66b9fbeef383
Frederik Kofoed Marqversen <frederik.kofoed.marqversen@cern.ch>
2019-07-04 15:52:34 +0200

    Added listings and commit

--- src/GitLab.js
+++ src/GitLab.js
@@ -15,67 +15,130 @@ export default class GitLab {
      * 
      * @param {string} token 
      */
-    constructor(token, branch="master", authorEmail="lhcvdm@cern.ch", authorUsername="lhcvdm"){
-        this._auth_headers = new Headers({
-            "Private-Token": token
-        });
+    constructor(token, branch = "master", authorEmail = "lhcvdm@cern.ch", authorUsername = "lhcvdm") {
+        this.token = token;
         this._branch = branch;
         this._authorEmail = authorEmail;
         this._authorUsername = authorUsername;
     }
 
-    _getURLFileName(experimentName, ipNum, fileName) {
-        return `${experimentName}/IP${ipNum}/${fileName}`;
+    _getURLFileName(campain, ipNum, fileName) {
+        return `${campain}/IP${ipNum}/${fileName}`;
     }
 
     /**
      * Returns the plaintext of the given file
      * 
-     * @param {string} experimentName 
+     * @param {string} campain 
      * @param {number} ipNum 
      * @param {string} fileName 
      */
-    async getFile(experimentName, ipNum, fileName){
-        return await (await gFetch(
-            `${URL_START}/repository/files/${
-                encodeURIComponent(this._getURLFileName(experimentName, ipNum, fileName))
-            }?ref=${this._branch}`,
-            {
-                headers: this._auth_headers
-            }
-        )).text()
-    }
 
-    
+    async readFile(campain, ipNum, fileName){
+        return await (
+            await gFetch(
+                `${URL_START}/repository/files/${
+                    encodeURIComponent(this._getURLFileName(campain, ipNum, fileName))
+                }/raw?ref=${this._branch}`, 
+                {headers: new Headers({'Private-Token': this.token})}
+            )
+        ).text();
+    };
+
     /**
      * 
-     * @param {string} experimentName 
+     * @param {string} campain 
      * @param {number} ipNum 
      * @param {string} fileName 
      * @param {string} commitMessage 
      * @param {string} fileText 
      */
-    async writeFile(experimentName, ipNum, fileName, commitMessage, fileText){
+    async writeFile(campain, ipNum, fileName, commitMessage, fileText) {
         await gFetch(
             `${URL_START}/repository/commits`,
             {
-                headers: this._auth_headers,
+                headers: new Headers({
+                    'Private-Token': this.token,
+                    'Content-Type': 'application/json'
+                }),
                 method: "POST",
                 body: JSON.stringify({
                     branch: this._branch,
                     commit_message: commitMessage,
                     author_email: this._authorEmail,
                     author_username: this._authorUsername,
-                    actions: [
-                        {
-                            action: "update",
-                            file_path: this._getURLFileName(experimentName, ipNum, fileName),
-                            content: fileText
-                        }   
-                    ]
+                    actions: [{
+                        action: "update",
+                        file_path: this._getURLFileName(campain, ipNum, fileName),
+                        content: fileText
+                    }]
                 })
             }
         )
+    }
 
+    /**
+     * 
+     * @param {string} campain 
+     * @param {number} ipNum 
+     * @param {string} fileName 
+     * @param {string} commitMessage 
+     */
+    async createFile(campain, ipNum, fileName, commitMessage) {
+        await gFetch(
+            `${URL_START}/repository/commits`,
+            {
+                headers: new Headers({
+                    'Private-Token': this.token,
+                    'Content-Type': 'application/json'
+                }),
+                method: "POST",
+                body: JSON.stringify({
+                    branch: this._branch,
+                    commit_message: commitMessage,
+                    author_email: this._authorEmail,
+                    author_username: this._authorUsername,
+                    actions: [{
+                        action: "create",
+                        file_path: this._getURLFileName(campain, ipNum, fileName),
+                        content: ''
+                    }]
+                })
+            }
+        )
+    }
+
+    async listCampains(){
+        return await this.listFiles('/');
+    }
+    async listIPs(campain){
+        return await this.listFiles(campain);
+    }
+
+    /**
+     * 
+     * @param {string} path 
+     */
+    async listFiles(path){
+        const perPage = 100;
+        const page = await gFetch(
+            `${URL_START}/repository/tree?ref=${this._branch}&per_page=${perPage}&page=1&path=${path}`,
+            {
+                headers: new Headers({'Private-Token': this.token})
+            }
+        )
+        let campainList = (await page.json()).map(x => x.name);
+        let lastPage = Math.ceil(page.headers.get('X-Total')/perPage);
+        for(let i=2; i<=lastPage; i++){
+            const page = await gFetch(
+                `${URL_START}/repository/tree?ref=${this._branch}&per_page=${perPage}&page=${i}&path=${path}`,
+                {
+                    headers: new Headers({'Private-Token': this.token})
+                }
+            );
+            let tmp = (await page.json()).map(x => x.name)
+            campainList = campainList.concat(tmp);
+        }
+        return campainList.filter(x => x!='readme.md');
     }
 }
--- src/testGitLab.js
+++ src/testGitLab.js
@@ -13,7 +13,7 @@ describe("GitLab", () => {
     })
 
     it("can get files", async () => {
-        expect(await gitlab.getFile(
+        expect(await gitlab.readFile(
             "201605_VdM",
             5,
             "HobbitCrossing.txt"
@@ -21,20 +21,37 @@ describe("GitLab", () => {
     })
 
     it("doesn't get incorrect files", async () => {
-        expect(await gitlab.getFile(
+        expect(await gitlab.readFile(
             "no_file",
             -9,
             "DoesntExist.txt"
         ).catch(() => -1)).toBe(-1)
     })
 
-    fit("can commit something", async () => {
+    it("can commit something", async () => {
         expect(await gitlab.writeFile(
             "201605_VdM",
             5,
             "TestFile.txt",
             "Test commit",
             Math.random().toString()
+        )).toBeUndefined()
+    })
+
+    it('listing campains', async () => {
+        expect(await gitlab.listCampains()).toEqual(jasmine.any(Array))
+    })
+
+    it("listing IPs", async () => {
+        expect(await gitlab.listIPs('201605_VdM')).toEqual(jasmine.any(Array))
+    })
+
+    xit("can create something", async () => {
+        expect(await gitlab.createFile(
+            "201605_VdM",
+            5,
+            Math.random().toString(),
+            "Test create",
         )).toBeUndefined()
     })
 })
