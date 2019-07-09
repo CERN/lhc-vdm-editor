155f3d60969e3a0a85abcfdc58973b63777942dd
Frederik Kofoed Marqversen <frederik.kofoed.marqversen@cern.ch>
2019-07-09 17:39:26 +0200

    Added the simulateBeamPath method to the VdM class

--- interpreter/script.js
+++ interpreter/script.js
@@ -6,8 +6,7 @@ $(document).ready(function () {
     /* let table = document.getElementById('table');
     table.content = source_commands; */
 
-    let file =
-        `0 INITIALIZE_TRIM IP(IP8) BEAM(BEAM1,BEAM2) PLANE(SEPARATION,CROSSING) UNITS(SIGMA)
+    let file = `0 INITIALIZE_TRIM IP(IP8) BEAM(BEAM1,BEAM2) PLANE(SEPARATION,CROSSING) UNITS(SIGMA)
 1 SECONDS_WAIT 20
 2 RELATIVE_TRIM IP8 BEAM1 CROSSING -3.375 SIGMA IP8 BEAM2 CROSSING 3.375 SIGMA
 3 SECONDS_WAIT 10
@@ -18,9 +17,9 @@ $(document).ready(function () {
 
     // Here I do stuff with the object!
     VdMObject = new VdMConstructor(file);
-    // console.log(VdMObject.lines);
-    VdMObject.addLine('10 THIS_IS_AN_ADDED_LINE', 2);
     console.log(VdMObject.lines);
+    // VdMObject.addLine('10 THIS_IS_AN_ADDED_LINE', 2);
+    // console.log(VdMObject.lines);
     // VdMObject.removeLine(2);
     // console.log(VdMObject.lines);
     // VdMObject.replaceLine('100 THIS_IS_A_REPLACED_LINE', 2)
@@ -35,6 +34,9 @@ class VdMConstructor {
     constructor(data) {
         let lineArr = data.split(/\n+/);
         this.lines = this._commandlinesToObjects(lineArr);
+        this._simulateBeamPath(this.lines);
+
+        // Some code that checks whether there is an INITIALIZE_TRIM line
     }
 
     /**
@@ -50,6 +52,7 @@ class VdMConstructor {
         });
         let middle = this._commandlinesToObjects([newLine]);
         this.lines = start.concat(middle, end);
+        this._simulateBeamPath(this.lines);
     }
     /**
      * @param {string} newLine
@@ -63,6 +66,7 @@ class VdMConstructor {
             return obj;
         })
         this.lines = start.concat(end);
+        this._simulateBeamPath(this.lines);
     }
     /**
      * @param {string} newLine
@@ -80,19 +84,31 @@ class VdMConstructor {
         let objArr = [];
         try {
             if (!Array.isArray(lineArr)) {
-                throw '_commandlinesToObjects takes an array! Got "' + typeof lineArr + '" with value: ' + lineArr
+                throw '_commandlinesToObjects takes an array! Got "' + typeof lineArr + '" with value: ' + JSON.stringify(lineArr)
             }
             for (let line of lineArr) {
+                // Deconstruct string into arguments
                 let tmp = line.trim().split(/\s+/);
                 if (tmp.length < 2) {
                     throw 'Command line has to include: "int command". Got: ' + tmp
                     // Maybe continue instead?
                 }
+
+                // Make object with areguments from string
                 let obj = {};
                 obj.index = parseInt(tmp[0]);
                 obj.command = tmp[1];
                 obj.args = tmp.slice(2);
 
+                // Initialise the simulated parameters to object
+                obj.beamPos = {
+                    'BEAM1': { 'CROSSING': 0.0, 'SEPARATION': 0.0 },
+                    'BEAM2': { 'CROSSING': 0.0, 'SEPARATION': 0.0 }
+                }
+                obj.stepTime = 0.0;
+                obj.realTime = 0.0;
+
+                // Add ovject to array
                 objArr.push(obj);
             }
         } catch (err) {
@@ -100,8 +116,30 @@ class VdMConstructor {
             console.log(err)
         }
         return objArr
-        // Maybe we do this?
-        // return objArr.length > 1 ? objArr : objArr[0];
+    }
+
+    /**
+     * 
+     * @param {*} objArr 
+     */
+    // Maybe 'updateBeamPath' more fitting. Do not know what to put in above
+    _simulateBeamPath(objArr) {
+        try {
+            let step = objArr[0];
+            this._commandHandler[step.command].apply(step, step.args);
+            if (objArr.length > 1) {
+                for (let i = 1; i < objArr.length; i++) {
+                    step = objArr[i];
+                    step.beamPos = objArr[i - 1].beamPos;
+                    step.stepTime = objArr[i - 1].stepTime;
+                    step.realTime = objArr[i - 1].realTime;
+                    this._commandHandler[step.command].apply(step, step.args);
+                }
+            }
+        } catch (err) {
+            // Do something on error
+            console.log(err)
+        }
     }
 
     _commandHandler = {
@@ -113,8 +151,8 @@ class VdMConstructor {
             if (!isFinite(waitTime)) {
                 throw 'Wait time has to be finite. Got: ' + duration
             }
-            stepTime += waitTime;
-            realTime += waitTime;
+            this.stepTime += waitTime;
+            this.realTime += waitTime;
         },
         'RELATIVE_TRIM': function (...actions) {
 
@@ -135,4 +173,5 @@ class VdMConstructor {
 
         }
     }
+
 }
