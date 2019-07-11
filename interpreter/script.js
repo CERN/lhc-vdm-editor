1fce39a80cfb528ffac0b772057e96f917641bd3
Frederik Kofoed Marqversen <frederik.kofoed.marqversen@cern.ch>
2019-07-11 16:07:30 +0200

    Finished the _parse function

--- interpreter/script.js
+++ interpreter/script.js
@@ -74,32 +74,45 @@ class VdMConstructor {
      * @param {Array} lineArr
      */
     _parse(data) {
-        let lineArr = data.split(/\n/);
+        let lineArr = data.split(/\n/).map(x => x.trim());
         let objArr = [];
+        let currentlineNum = 0;
+        let hasEnded = false;
+        let IP = [];
+        let beams = [];
+        let planes = [];
+        let planesContainer = [];
+        let units = [];
+        let fitTypes = ['GAUSSIAN', 'GAUSSIAN_PLUS_CONSTANT'];
+
         try {
             if (!Array.isArray(lineArr)) {
-                throw new SyntaxError({}'_commandlinesToObjects takes an array! Got "' + typeof lineArr + '" with value: ' + JSON.stringify(lineArr))
+                throw Error('_commandlinesToObjects takes an array! Got "' + typeof lineArr + '" with value: ' + JSON.stringify(lineArr))
             }
             for (let i = 0; i < lineArr.length; i++) {
                 // Deconstruct string into arguments
-                let tmp = lineArr[i].trim().split(/\s+/);
+                let line = lineArr[i].split(/\s+/);
                 let obj = {};
-                // Check for valid lines
-                if (!isInteger(tmp[0])) {
-                    if (tmp[0] == '') {
-                        // Do nothing
-                    } else if (tmp[0].charAt(0) == '#') {
-                        obj.comment = tmp[0];
+                // Check line syntax
+                if (!isInteger(line[0])) {
+                    if (line[0] == '') {
+                        obj.type = 'empty';
+                    } else if (line[0].charAt(0) == '#') {
+                        obj.type = 'comment';
+                        obj.comment = line[0];
                     } else {
                         throw new SyntaxError(i, 'Line has to be of the type "#COMMENT", "INT COMMAND", or "EMPTY_LINE"')
                     }
+                } else if (hasEnded){
+                    throw new SyntaxError(i, 'Encountered command line "' + lineArr[i] + '" after END_SEQUENCE')
+                } else if (parseInt(line[0]) != currentlineNum) {
+                    throw new SyntaxError(i, 'Incorrect line numbering. Expected ' + currentlineNum + ' but got ' + parseInt(line[0]))
                 } else { // Create object from command line
-                    if (tmp.length < 2) {
-                        throw new SyntaxError(i, 'Command line has to include: "int + command" but got: ' + tmp);
-                    }
-                    obj.command = tmp[1];
-                    obj.args = tmp.slice(2);
-                    try{_validateArgs(obj)} catch(err){throw new SyntaxError(i, err)}
+                    currentlineNum ++;
+                    obj.type = 'command';
+                    obj.command = line[1];
+                    obj.args = line.slice(2);
+                    try { validateArgs(obj) } catch (err) { throw new SyntaxError(i, err) }
                 }
                 objArr.push(obj);
             }
@@ -107,41 +120,105 @@ class VdMConstructor {
             throw Error('Error while parsing line' + err.line + '.\n' + err.message)
         }
         return objArr
-    }
 
-    _validateArgs(obj) {
-        if (!commandHandler[obj.command]) {
-            throw 'Invalid command ' + obj.command
-        } else {
-            
-        }
-        let commandHandler = {
-            'INITIALIZE_TRIM': function (ips, beams, planes, units) {
-
-            },
-            'SECONDS_WAIT': function (arg) {
-
-            },
-            'RELATIVE_TRIM': function (...actions) {
-
-            },
-            'ABSOLUTE_TRIM': function (...actions) {
-
-            },
-            'START_FIT': function (plane, type) {
-
-            },
-            'END_FIT': function (plane) {
-
-            },
-            'END_SEQUENCE': function (actions) {
-
-            },
-            'MESSAGE': function (message) {
-
+        function validateArgs(obj) {
+            function checkTrim(obj) {
+                for(let i=0; i<obj.args.length; i+=5){
+                    if (!IP.includes(obj.args[i])){
+                        throw 'Invalid TRIM command. Expected ' + IP + ' but got ' + obj.args[i]
+                    }
+                    if (!beams.includes(obj.args[i+1])){
+                        throw 'Invalid TRIM command. Expected beam in' + beams + ' but got ' + obj.args[i+1]
+                    }
+                    if (!beams.includes(obj.args[i+2])){
+                        throw 'Invalid TRIM command. Expected plane in' + planes + ' but got ' + obj.args[i+2]
+                    }
+                    if (!isFinite(obj.args[i+3])){
+                        throw 'Invalid TRIM command. Amount has to be finite but got ' + obj.args[i+3]
+                    }
+                    if (!beams.includes(obj.args[i+4])){
+                        throw 'Invalid TRIM command. Expected unit in' + units + ' but got ' + obj.args[i+4]
+                    }
+                }
+            }            
+            let commandHandler = {
+                'INITIALIZE_TRIM': function (obj) {
+                    if (currentlineNum != 0){
+                        throw 'Invalid INITIALIZE_TRIM command. Must occur on line zero'
+                    }
+                    function getInnerBracket(str, type){
+                        let match = str.match(new RegExp('^' + type + '\\((.*)\\)$'))[1].split(',');
+                        if (!match){
+                            throw 'Invalid INITIALIZE_TRIM command. Must include ' + type + '(...)'
+                        }
+                        if (match.length > 2){
+                            throw 'Invalid INITIALIZE_TRIM command.' + type + ' can include at most 2 arguments'
+                        }
+                        return match;
+                    }
+                    IP = getInnerBracket(obj.args[0], 'IP');
+                    beams = getInnerBracket(obj.args[1], 'BEAM');
+                    planes = getInnerBracket(obj.args[2], 'PLANE');
+                    units = getInnerBracket(obj.args[3], 'UNITS');
+                    if (IP.length != 1){
+                        throw 'Invalid INITIALIZE_TRIM command. Must include exactly one IP'
+                    }
+                    if (obj.args.length != 4) {
+                        throw 'Invalid INITIALIZE_TRIM command. Too many arguments. Overflow: ' + obj.args.slice(4)
+                    }
+                },
+                'SECONDS_WAIT': function (obj) {
+                    if (obj.args.length != 1){
+                        throw 'Invalid SECONDS_WAIT command. Expected exactly one argument but got ' + obj.args
+                    }
+                    if (!isFinite(obj.args)) {
+                        throw 'Invalid SECONDS_WAIT command. Argument must be finte but got ' + obj.args
+                    }
+                },
+                'RELATIVE_TRIM': function (obj) {
+                    checkTrim(obj)
+                },
+                'ABSOLUTE_TRIM': function (obj) {
+                    checkTrim(obj)
+                },
+                'START_FIT': function (obj) {
+                    if (!planes.includes(obj.args[0])){
+                        throw 'Invalid START_FIT command. Expected plane in ' + planes + ' but got ' + obj.ars[0]
+                    }
+                    if (!fitTypes.includes(obj.args[1])) {
+                        throw 'Invalid START_FIT command. Expected fit type in ' + fitTypes + ' but got ' + obj.ars[1]
+                    }
+                    if (obj.args.length != 2) {
+                        throw 'Invalid START_FIT command. Too many arguments. Overflow: ' + obj.args.slice(2)
+                    }
+                    planesContainer = planes;
+                    planes = [obj.args[0]];
+                },
+                'END_FIT': function (obj) {
+                    if (obj.args){
+                        throw 'Invalid END_FIT command. No arguments allowed but got ' + obj.args
+                    }
+                    planes = planesContainer;
+                },
+                'END_SEQUENCE': function (obj) {
+                    if (obj.args){
+                        throw 'Invalid END_SEQUENCE command. No arguments allowed but got ' + obj.args
+                    }
+                    hasEnded = true;
+                },
+                'MESSAGE': function (obj) {
+                    // Do nothing
+                }
+            }
+            if (!commandHandler[obj.command]) {
+                throw 'Unknown command "' + obj.command + '" encountered'
+            }
+            else {
+                commandHandler[obj.command].apply(obj);
             }
         }
     }
+
     /**
      * 
      * @param {*} objArr 
