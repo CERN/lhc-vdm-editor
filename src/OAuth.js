284556442d630a1a56d7743ed25cd0b68b9018bf
ThomasHickman <Thomas.Hickman42@gmail.com>
2019-07-23 17:23:45 +0200

    Add OAuth

--- src/OAuth.js
+++ src/OAuth.js
@@ -1,7 +1,26 @@
-export default class OAuth {
-    getCredentials (){
-        window.location = `https://oauth.web.cern.ch/OAuth/Authorize?scope=read%3Auser&response_type=code&redirect_uri=${
-            encodeURIComponent('http://lhcvdm.web.cern.ch/lhcvdm/oauth/&client_id=lhhvdm.cern.ch')
-        }`
+// @ts-check
+
+export class OAuthError extends Error {}
+
+export class OAuth {
+    getCredentials() {
+        const clientId = "d26077e78d7fe7918a6f0e7eff276209a2b558975e34e2fa5709f4236a27d26a";
+        const redirectURI = "https://lhcvdm.web.cern.ch/lhcvdm/";
+        const oauthState = Math.floor(Math.random()*(2**32)).toString();
+        localStorage.setItem("oauth_state", oauthState)
+        // @ts-ignore
+        window.location = `https://gitlab.cern.ch/oauth/authorize?client_id=${clientId
+            }&redirect_uri=${redirectURI}&state=${oauthState}`
+    }
+
+    getAccessToken() {
+        const access_token = window.location.hash.match(/access_token=(.*?)(?:&|$)/)[1];
+        const state = window.location.hash.match(/state=(.*?)(?:&|$)/)[1];
+
+        if(state !== localStorage.getItem("oauth_state")){
+            throw new OAuthError("OAuth state parameter doesn't match stored state");
+        }
+
+        return access_token;
     }
 }
--- src/main.js
+++ src/main.js
@@ -3,8 +3,24 @@ import GitLab from "./GitLab.js"
 import { getJSON } from "./HelperFunctions.js"
 import OverallEditor from "./OverallEditor.js"
 import "../extern/ace.js"
+import {OAuth} from "./OAuth.js"
+
 $(async () => {
-    const token = (await getJSON("./secrets.json")).token;
+    let token;
+
+    if(window.location.origin == "https://lhcvdm.web.cern.ch"){
+        const oa = new OAuth();
+        if(window.location.hash.includes("access_token")){
+            token = oa.getAccessToken();
+        }
+        else{
+            oa.getCredentials()
+        }
+    }
+    else{
+        token = (await getJSON("./secrets.json")).token;
+    }
+
     const filePath = "201605_VdM/IP5/HobbitCrossing.txt";
 
     const gl = new GitLab(token, "vdm-editor-test");
