a6b836defde05c80f0ddd62ca7e7c26df928bf1d
Thomas Hickman <Thomas.Hickman42@gmail.com>
2019-08-06 10:07:46 +0200

    More work on CodeEditor tests

--- src/components/CodeEditor.test.js
+++ src/components/CodeEditor.test.js
@@ -1,4 +1,5 @@
 import CodeEditor from "./CodeEditor.js"
+import { range, wait, asyncMap, addLineNumbers } from "../HelperFunctions.js"
 
 const simpleFile = '0 INITIALIZE_TRIM IP(IP1) BEAM(BEAM1,BEAM2) PLANE(SEPARATION) UNITS(SIGMA)\n1 SECONDS_WAIT 1.0\n2 END_SEQUENCE';
 const simpleFileWithComments = '#start\n0 INITIALIZE_TRIM IP(IP1) BEAM(BEAM1,BEAM2) PLANE(SEPARATION) UNITS(SIGMA)\n#middle\n1 SECONDS_WAIT 1.0\n2 END_SEQUENCE';
@@ -26,20 +27,35 @@ function getKeyboardEvent(keyCode){
 
 describe("CodeEditor", () => {
     /** @type CodeEditor */
-    let editor;
-    xit("autocompletes correctly", () => {
-        editor.editor.execCommand("startAutocomplete");
-        editor.editor.onCommandKey(getKeyboardEvent(13/*Enter*/), 0, 13)
+    let ce;
+    beforeEach(() => {
+        ce = new CodeEditor();
+        ce.style.visibility = "invisible";
+        document.body.append(ce);
+    })
+    afterEach(() => {
+        document.body.removeChild(ce);
     })
 
-    describe("parsing and deparsing", () => {
-        /** @type {CodeEditor}  */
-        let ce;
+    fit("autocompletes correctly", async () => {
+        ce.editor.execCommand("startAutocomplete");
 
-        beforeEach(() => {
-            ce = new CodeEditor();
+        await asyncMap(range(6), async () => {
+            ce.editor.onCommandKey(getKeyboardEvent(13/*Enter*/), 0, 13);
+            await wait(0);
         })
 
+        expect(ce.value).toBe(
+`0 INITIALIZE_TRIM IP(IP1) BEAM(BEAM1) PLANE(CROSSING) UNITS(SIGMA)
+1 RELATIVE_TRIM IP1 BEAM1 CROSSING 0.0 SIGMA
+2 END_SEQUENCE
+`
+        )
+
+        console.log(ce.value);
+    })
+
+    xdescribe("parsing and deparsing", () => {
         it("parses and deparses a simple file", () => {
             ce.value = simpleFile;
 
