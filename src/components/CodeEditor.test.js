b60c5174d1baea4241d807599ca37a9e2dfe0657
Thomas Hickman <Thomas.Hickman42@gmail.com>
2019-08-13 10:37:22 +0200

    Add CodeEditor tests

--- src/components/CodeEditor.test.js
+++ src/components/CodeEditor.test.js
@@ -1,8 +1,10 @@
 import CodeEditor from "./CodeEditor.js"
+import { range, wait, asyncMap, addLineNumbers } from "../HelperFunctions.js"
 
-const simpleFile = '0 INITIALIZE_TRIM IP(IP1) BEAM(BEAM1,BEAM2) PLANE(SEPARATION) UNITS(SIGMA)\n1 SECONDS_WAIT 1.0\n2 END_SEQUENCE\n';
-const simpleFileWithComments = '#start\n0 INITIALIZE_TRIM IP(IP1) BEAM(BEAM1,BEAM2) PLANE(SEPARATION) UNITS(SIGMA)\n#middle\n1 SECONDS_WAIT 1.0\n2 END_SEQUENCE\n';
+const simpleFile = '0 INITIALIZE_TRIM IP(IP1) BEAM(BEAM1,BEAM2) PLANE(SEPARATION) UNITS(SIGMA)\n1 SECONDS_WAIT 1.0\n2 END_SEQUENCE';
+const simpleFileWithComments = '#start\n0 INITIALIZE_TRIM IP(IP1) BEAM(BEAM1,BEAM2) PLANE(SEPARATION) UNITS(SIGMA)\n#middle\n1 SECONDS_WAIT 1.0\n2 END_SEQUENCE';
 
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000000;
 
 function getKeyboardEvent(keyCode){
     var keyboardEvent = document.createEvent("KeyboardEvent");
@@ -25,21 +27,40 @@ function getKeyboardEvent(keyCode){
 }
 
 describe("CodeEditor", () => {
+    beforeAll(() => {
+        const makeInvisibleStyle = document.createElement("style");
+        makeInvisibleStyle.innerText = ".ace_autocomplete{visibility: hidden !important}";
+        document.body.appendChild(makeInvisibleStyle);
+    })
     /** @type CodeEditor */
-    let editor;
-    xit("autocompletes correctly", () => {
-        editor.editor.execCommand("startAutocomplete");
-        editor.editor.onCommandKey(getKeyboardEvent(13/*Enter*/), 0, 13)
+    let ce;
+    beforeEach(() => {
+        ce = new CodeEditor();
+
+        ce.style.visibility = "hidden";
+
+        document.body.append(ce);
+    })
+    afterEach(() => {
+        document.body.removeChild(ce);
+    })
+
+    it("autocompletes correctly", async () => {
+        ce.editor.execCommand("startAutocomplete");
+
+        await asyncMap(range(6), async () => {
+            ce.editor.onCommandKey(getKeyboardEvent(13/*Enter*/), 0, 13);
+            await wait(0);
+        })
+
+        expect(ce.rawValue).toBe("RELATIVE_TRIM IP1 BEAM1 CROSSING 0.0 SIGMA")
+    })
+
+    it("adds header and footers to blank file", () => {
+        expect(ce.value).toBe("0 INITIALIZE_TRIM IP() BEAM() PLANE() UNITS()\n1 END_SEQUENCE\n")
     })
 
     describe("parsing and deparsing", () => {
-        /** @type {CodeEditor}  */
-        let ce;
-
-        beforeEach(() => {
-            ce = new CodeEditor();
-        })
-
         it("parses and deparses a simple file", () => {
             ce.value = simpleFile;
 
