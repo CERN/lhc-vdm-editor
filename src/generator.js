3ea7be4a1bac12a7df5adaada701deae18e3dca1
Frederik Kofoed Marqversen <frederik.kofoed.marqversen@cern.ch>
2019-08-15 13:50:58 +0200

    Added generator class

--- src/generator.js
+++ src/generator.js
@@ -1,3 +1,19 @@
+function linspace(start, end, num, includeEnd = true) {
+    if (!Number.isInteger(num) || num < 1) throw new Error('Number has to be an integer grater than 0');
+
+    const dist = (end - start) / (includeEnd ? num - 1 : num);
+    let result = new Array(num);
+
+    result[0] = start;
+    for (let i = 1; i < num - 1; i++) {
+        result[i] = result[i - 1] + dist
+    }
+    if (includeEnd) result[num - 1] = end;
+
+    return result
+}
+
+
 export default class Generator {
     /**
      * @param {string} ip
@@ -6,62 +22,170 @@ export default class Generator {
         this.ip = ip;
 
         this.functions = {
-            linear: function (startPos, endPos, runtime, t) {
-                return t <= runtime ? startPos + t * (endPos - startPos) / runtime : 0
+            linear: function (startPos, endPos, endTime) {
+                return (t) => {
+                    if (t < endTime) return startPos + t * (endPos - startPos) / endTime;
+                    else return 0;
+                }
             },
-            zero: () => 0,
+            period: (period) => (t) => (t % period < period / 2) ? 1 : 0,
+            step: function (startTime, endTime) {
+                return (t) => (t >= startTime && t <= endTime) ? 1 : 0
+            },
+            // These are in fact redundant since numbers are taken to be constant functions but are included for completeness
+            zero: () => (t) => 0, // Must be used as this.functions.zero() which returns the zerofunction
+            constant: (value) => (t) => value,
         };
 
-        let func1 = [(t) => this.functions.linear(5, -5, 750, t), this.functions.zero];
-        let func2 = [(t) => this.functions.linear(-5, 5, 750, t), this.functions.zero];
-        this.test = this.generate(func1, func2, 30, 1000);
+
+        /* These are all examples of how to efficiently use the generator.
+        Functions as well as hardcoded posArrays can easily be added.
+
+        let funcArr
+        let func
+
+        funcArr = [this.functions.linear(-5, 5, 100), , this.functions.linear(5, -5, 100),]; // no crossing plane functions supplied
+        this.template1 = this.generateFromFunction(funcArr, 50, 100);
+
+        funcArr = Array(4);
+        func = this.prodFunc(this.functions.period(6), this.functions.step(40, 60)); // periodic function on the interval [40, 60]
+        funcArr[0] = this.sumFunc(-1, func, this.functions.constant(1));
+        funcArr[2] = 1;
+        this.template2 = this.generateFromFunction(funcArr, 50, 100);
+
+        let arr1 = linspace(3, -3, 5).map(x => Array(3).fill(x)).flat();
+        arr1.unshift(0);
+        arr1.push(0);
+        let arr2 = [0, 4, 3, 1.5, 3, 1.5, 0, 1.5];
+        arr2 = arr2.concat([0]).concat(arr2.map(x => -x).reverse());
+        this.template3 = this.generateFromArray([arr1, , arr2, ,], 30); 
+        */
+    }
+
+    sumFunc(...funcs) {
+        return (t) => funcs.map(func => {
+            if (typeof func == 'number') return func
+            else return func(t)
+        }).reduce((acc, cur) => acc + cur, 0)
+    }
+    prodFunc(...funcs) {
+        return (t) => funcs.map(func => {
+            if (typeof func == 'number') return func
+            else return func(t)
+        }).reduce((acc, cur) => acc * cur, 1)
+    }
+    delayFunc(func, startTime) {
+        return (t) => (t >= startTime) ? func(t - startTime) : 0
     }
 
     /**
-     * @param {any} funcArrBeam1
-     * @param {any} funcArrBeam2
-     * @param {number} stepTime
-     * @param {number} totTime
+     * @param {function[]} inpFuncArr
+     * @param {number} stepNum
+     * @param {number} endTime
      */
-    generate(funcArrBeam1, funcArrBeam2, stepTime, totTime) {
-        // beam-N-func is an array with [funcSep, funcCross] that takes time t and returns absolute position
-        let funcArr = funcArrBeam1.concat(funcArrBeam2);
-        let description = [
+    generateFromFunction(inpFuncArr, stepNum, endTime) {
+        /* funcArr is an array with 4 functions as specified in "desciption".
+        Each function will be evaluated from 0 to endTime.
+        If a function is undefined it is simply ignored (zero function).
+        If a function is a number, it is taken to be the constant function. */
+        const funcArr = inpFuncArr.map((x, i) => {
+            if (typeof x == 'function') return x
+            else if (typeof x == 'number') return this.functions.constant(x)
+            else if (Array.isArray(x)) return this.sumFunc(...x)
+            else throw new Error('invalid entry')
+        });
+        const description = [
             'BEAM1 SEPARATION',
             'BEAM1 CROSSING',
             'BEAM2 SEPARATION',
             'BEAM2 CROSSING'
         ]
 
+        const stepTime = endTime / stepNum;
+        let currentTime = 0;
+        let accWaitTime = 0;
+        let prevPos = [0, 0, 0, 0]
+
         let lineArr = [];
-        lineArr.push(`0 INITIALIZE_TRIM IP(${this.ip}) BEAM(BEAM1,BEAM2) PLANE(SEPARATION,CROSSING) UNITS(SIGMA)`);
-        lineArr.push(`1 SECONDS_WAIT ${stepTime}`);
+        lineArr.push('SECONDS_WAIT ' + stepTime);
 
-        let lineNum = 2;
-        let time = 0;
-        let pos = [0, 0, 0, 0]
-        while (time <= totTime) {
-            let line = `${lineNum} RELATIVE_TRIM`;
-            let addLine = false;
+        while (currentTime <= endTime) {
+            let line = '';
 
-            for (let i = 0; i < description.length; i++) {
-                let relStep = (funcArr[i](time + stepTime) - pos[i]).toFixed(2);
+            funcArr.forEach((func, i) => {
+                const relStep = (func(currentTime) - prevPos[i]).toFixed(2);
                 if (parseFloat(relStep) != 0) {
                     // Maybe add some test if we want to use absolute trim instead
-                    pos[i] += parseFloat(relStep);
+                    prevPos[i] += parseFloat(relStep);
                     line += ` ${this.ip} ${description[i]} ${relStep} SIGMA`;
-                    addLine = true;
                 };
-            };
+            })
 
-            if (addLine) lineArr.push(line);
-            lineArr.push(`${lineNum + 1} SECONDS_WAIT ${stepTime}`);
+            if (line != '') {
+                if (accWaitTime > 0) {
+                    lineArr.push('SECONDS_WAIT ' + accWaitTime);
+                    accWaitTime = 0;
+                }
+                lineArr.push('RELATIVE_TRIM' + line);
+            }
 
-            lineNum += 2;
-            time += stepTime;
+            accWaitTime += stepTime;
+            currentTime += stepTime;
         };
+        if (accWaitTime > 0) lineArr.push('SECONDS_WAIT ' + accWaitTime);
 
-        lineArr.push(`${lineNum + 1} END_SEQUENCE`);
+        prevPos.forEach((x, i) => {
+            if (x != 0) {
+                lineArr.push(`ABSOLUTE_TRIM ${this.ip} ${description[i]} 0.0 SIGMA`);
+            }
+        })
+
+        lineArr.push('SECONDS_WAIT ' + stepTime);
+        return lineArr.join('\n');
+    }
+
+    generateFromArray(arr, waitTime) {
+        /* arr is an array with 4 position-arrays as specified in "desciption".
+        The position arrays are evaluated simultaneously, one entry at a time and spaced waitTime apart
+        If an entry is undefined it is taken to be zero.
+        (it is thus allowed for the arrays to have different lengths)
+        Similarily if an array is undefined. */
+        const description = [
+            'BEAM1 SEPARATION',
+            'BEAM1 CROSSING',
+            'BEAM2 SEPARATION',
+            'BEAM2 CROSSING'
+        ]
+        const sequenceLength = arr.reduce((acc, cur) => Math.max(acc, cur.length), 0);
+
+        let lineArr = [];
+        lineArr.push('SECONDS_WAIT ' + waitTime);
+        let prevPos = [0, 0, 0, 0]
+
+        for (let index = 0; index < sequenceLength; index++) {
+            let line = '';
+
+            arr.forEach((posArr, i) => {
+                const newPos = posArr[index] || 0;
+                const relStep = newPos - prevPos[i];
+                if (relStep != 0) {
+                    // Maybe add some test if we want to use absolute trim instead
+                    prevPos[i] += relStep;
+                    line += ` ${this.ip} ${description[i]} ${relStep} SIGMA`;
+                };
+            })
+
+            if (line != '') {
+                lineArr.push('RELATIVE_TRIM' + line);
+                lineArr.push('SECONDS_WAIT ' + waitTime);
+            }
+        }
+
+        prevPos.forEach((x, i) => {
+            if (x != 0) {
+                lineArr.push(`ABSOLUTE_TRIM ${this.ip} ${description[i]} 0.0 SIGMA`);
+            }
+        })
 
         return lineArr.join('\n');
     }
