79b5e6f725cf2560c8a1538e5df3a278b4c4fad5
ThomasHickman <Thomas.Hickman42@gmail.com>
2019-08-21 14:46:53 +0200

    Stop raw-editor opening by default

--- src/components/CodeEditor.test.js
+++ src/components/CodeEditor.test.js
@@ -26,7 +26,7 @@ describe("CodeEditor", () => {
     beforeEach(() => {
         ce = new CodeEditor();
         ce.VdM = new VdM(DEFAULT_BEAM_PARAMS, "IP1");
-
+        ce.ip = "IP1"
         ce.style.visibility = "hidden";
 
         document.body.append(ce);
