8ee8bfef8623866353d8459742cb3ecac515d57f
Frederik Kofoed Marqversen <frederik.kofoed.marqversen@cern.ch>
2019-09-10 15:21:06 +0200

    Spelling mistake fixed

--- src/EasterEgg.js
+++ src/EasterEgg.js
@@ -26,7 +26,7 @@ const template = `
             { Frederik K. M., Thomas K. H. } &#8838; { CERN lunches } &#8838; { CERN Summer Students 2019 }
         </p>
         <p>
-            We hope you find the application usefull
+            We hope you find the application useful
         </p>
     </div>
 `;
